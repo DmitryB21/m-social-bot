0ec90a03581bed369252c410839c22eff7824478
Dmitry Blyukherov <d.blukherov@gmail.com>
2025-05-21 12:36:23 +0300

    m-social-bot  initial commit

---
+++ src/main/java/com/github/dmitryb21/msocialbot/MSocialBotApplication.java
@@ -0,0 +1,13 @@
+package com.github.dmitryb21.msocialbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MSocialBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MSocialBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/dmitryb21/msocialbot/MSocialBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.dmitryb21.msocialbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MSocialBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
